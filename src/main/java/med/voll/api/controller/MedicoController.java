824accff4762dddc09ce21eb48909d0a39a19ec0
PedroAz <pdegutty@gmail.com>
2022-11-21 17:30:39 -0300

    ok

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,70 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size=10, page=0, sort ={"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico  = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    //EXCLUSÃO TOTAL
+//    @DeleteMapping("/{id}")
+//    public void deletar (@PathVariable (value ="id") Long id){
+//        repository.deleteById(id);
+//
+//    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deletar (@PathVariable (value ="id") Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+
+    }
+
+    @GetMapping ("/{id}")
+    @Transactional
+    public void ativar (@PathVariable (value ="id") Long id){
+        var medico = repository.getReferenceById(id);
+        medico.ativar();
+
+    }
+
+}
+
